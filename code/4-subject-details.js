b752e036818bd85324dd74a390da55bf686289fe
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-07-25 10:39:54 +1200

    wezrxvubjnluktetjk,jk.tg

--- code/4-subject-details.js
+++ code/4-subject-details.js
@@ -6,14 +6,22 @@ let subject = {
   name: 'DigiTech',
   room: 'Room 9',
   students: 150,
+  teacher: 'Random middle-aged man'
 }
 
 function showSubject() {
-  alert(subject.name)
+  alert(subject.name + ' in ' + subject.room + ' with ' + subject.teacher )
 }
 
 function changeSubject() {
   let newName = 'Typing Skills'
 
   subject.name = newName
+}
+
+function changeTeacher () {
+let newTeacher = prompt ("What's your teachers name?")
+
+subject.teacher = newTeacher
+
 }
